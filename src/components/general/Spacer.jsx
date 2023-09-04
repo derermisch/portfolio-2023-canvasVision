7bd4a4606bd56759b7a213284d7a3a67f8b218bb
Fabian Ermisch <fabian.ermisch@googlemail.com>
2023-09-04 15:31:00 +0200

    code cleanup for spacer

--- src/components/general/Spacer.jsx
+++ src/components/general/Spacer.jsx
@@ -8,98 +8,9 @@ function urlFor(source) {
     return builder.image(source)
 }
 
-import { getCurrentLightMode } from "../../utils/darkmode"
-
 // svg-code from https://app.haikei.app/. Dimensions: 1000 x 100
 export default function Spacer({ spacerBg, showLower = true }) {
     const containerRef = useRef(null)
-
-    // useEffect(() => {
-    //     if (!containerRef.current) return
-    //     if (!spacerRes) return
-    //     if (!targetElementClassName) return
-    //     if (!document.querySelector(targetElementClassName)) return
-
-    //     let myTargetRect = document.querySelector(targetElementClassName).getBoundingClientRect()
-
-    //     /** @type{HTMLElement} */
-    //     const container = containerRef.current
-    //     const lowerSpacer = container.lastChild
-    //     let spacerCode = spacerRes.spacerSvgCode
-    //     let initialSetup = true
-
-    //     const adjustSpacerCodeBasedOnLightMode = () => {
-    //         let newColor = "#22222"
-    //         getCurrentLightMode() === "dark" ? newColor = "#EFE9E7" : newColor = "#272727"
-    //         spacerCode = spacerCode.replace(/fill*=["'][^"']*["']/i, `fill="${newColor}"`);
-    //     }
-    //     adjustSpacerCodeBasedOnLightMode()
-
-    //     const setup = () => {
-    //         if (!initialSetup) {
-    //             const tmpRect = document.querySelector(targetElementClassName).getBoundingClientRect()
-    //             if (tmpRect.width === myTargetRect.width && tmpRect.height === myTargetRect.height) {
-    //                 return
-    //             }
-    //             myTargetRect = document.querySelector(targetElementClassName).getBoundingClientRect()
-    //         }
-    //         else {
-    //             myTargetRect = document.querySelector(targetElementClassName).getBoundingClientRect()
-    //             initialSetup = false
-    //         }
-    //         // console.log("setup")
-    //         const svg64 = window.btoa(spacerCode)
-    //         container.childNodes.forEach(child => {
-    //             child.style.backgroundImage = `url('data:image/svg+xml;base64,${svg64}')`
-    //             child.style.width = document.body.clientWidth + "px"
-    //         })
-    //         lowerSpacer.style.rotate = "180deg"
-    //         if (!showLower) {
-    //             lowerSpacer.style.display = "none"
-    //             container.style.top = myTargetRect.height + myTargetRect.top + Number(scrollY) + "px"
-    //         }
-    //         else {
-    //             container.style.top = myTargetRect.height + myTargetRect.top + Number(scrollY) - container.getBoundingClientRect().height / 2 + "px"
-    //         }
-    //     }
-    //     setup()
-
-    //     const onLightModeChanged = () => {
-    //         adjustSpacerCodeBasedOnLightMode()
-    //         initialSetup = true
-    //         setup()
-    //     }
-
-    //     // Has to also fire on dataFetched, because fetched data will change the size of the target Element
-    //     window.addEventListener("customResize", setup)
-    //     window.addEventListener("notifyLightMode", onLightModeChanged)
-    //     window.addEventListener("dataFetched", setup)
-
-    //     return () => {
-    //         window.removeEventListener("customResize", setup)
-    //         window.removeEventListener("notifyLightMode", onLightModeChanged)
-    //         window.removeEventListener("dataFetched", setup)
-    //     }
-    // }, [containerRef, spacerRes, targetElementClassName])
-
-    // useEffect(() => {
-    //     if (!containerRef.current) return
-    //     if (!spacerRes) return
-
-    //     /** @type {HTMLDivElement} */
-    //     const container = containerRef.current
-    //     const spacerCode = spacerRes.spacerSvgCode
-    //     const lowerSpacer = container.lastChild
-    //     const svg64 = window.btoa(spacerCode)
-    //     container.childNodes.forEach(child => {
-    //         child.style.backgroundImage = `url('data:image/svg+xml;base64,${svg64}')`
-    //         // child.style.width = document.body.clientWidth + "px"
-    //     })
-    //     lowerSpacer.style.rotate = "180deg"
-    //     if (!showLower) {
-    //         lowerSpacer.style.display = "none"
-    //     }
-    // }, [containerRef, spacerRes])
 
     useEffect(() => {
         (async () => {
@@ -114,8 +25,6 @@ export default function Spacer({ spacerBg, showLower = true }) {
             <div />
             <p className="spacer--warpText">### Hyperspace Warp ###</p>
             <div />
-            {/* <div className="spacer--upper"></div>
-            <div className="spacer--lower"></div> */}
         </div>
         : <ClipLoader
             className="clipLoader" />
